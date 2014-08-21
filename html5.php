e61f5f666609012b82246b55e6594a38ad3ce621
maembla <maembla@gmail.com>
2014-08-21 18:47:57 -0300

    Este es mi primer archivo subido el 21/08

---
+++ html5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+	</head>
+
+	<body>
+		<header style="border-image:url(flordelavida.jpg) 1 12 1 12 round round;width:100px; height:100px;"><p style="color: blue;opacity:3.0;margin:0;">Cabecera</p></header>
+		<nav style="border:1px solid #000000;">Aca va el Men&uacute;</nav>
+		<article style="border:1px solid #000000;">Contenido del articulo</article><section style="border:1px solid blue;">Secciones</section></article>
+		<aside style="border:1px solid green;">
+			<!--<audio src="GUNS'N ROSES - THE UNFORGIVEN.MP3" autoplay controls></audio>-->
+			<!--<object type="application/mp3" data="GUNS'N ROSES - THE UNFORGIVEN.MP3">
+				<param name="src" value="GUNS'N ROSES - THE UNFORGIVEN.MP3">
+			</object>-->
+			<SCRIPT Language="php">
+				echo ("Lo que sea!\n");
+			</SCRIPT>
+		</aside>
+		<footer style="border:1px solid #000000;">Pie de p&aacute;gina</footer>
+	</body>
+</html>
